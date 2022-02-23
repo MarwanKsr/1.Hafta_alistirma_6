ae68fbfd225544cbf04b8d843952ad9a9b28f048
Marwan <Marwan.kaseer1997@gmail.com>
2022-02-23 11:29:29 +0300

    Program is done

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace alistirma_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Kilonuz giriniz: ");
+            float kilo = int.Parse(Console.ReadLine());
+            Console.Write("Boyunuz giriniz: ");
+            float Boy = int.Parse(Console.ReadLine());
+            Boy /= 100;
+            float vucut_kutle = kilo / (Boy*Boy);
+            Console.WriteLine("Vucut kutle indeksiniz {0} kg/metrekare",vucut_kutle);
+        }
+    }
+}
